661f074eacf0b571d4f74e98d8c4c2a48f00852f
Biswa Mitra Barua <biswamitra960@gmail.com>
2024-10-18 20:34:20 +0600

    changed some needs

--- src/Post.jsx
+++ src/Post.jsx
@@ -4,7 +4,7 @@ export default function Post({post}){
     return (
         <div className='post'>
             <h5>Title: {title}</h5>
-            <p><small>UserId: {title}</small></p>
+            <p><small>UserId: {userId}</small></p>
             <p><small>PostId: {id}</small></p>
             <p>{body}</p>
         </div>
